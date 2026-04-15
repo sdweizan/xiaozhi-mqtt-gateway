5be858960d7b23b9c4e647b45f6d3ec6a687b7c6
sdweizan <gaofeng@caihong.com>
2026-04-15 09:05:59 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git pull
 IMAGE_NAME=registry-vpc.cn-hangzhou.aliyuncs.com/caihong5g/xiaozhi-gateway
-docker build -f Dockerfile -t $IMAGE_NAME:latest .
+docker build --no-cache -f Dockerfile -t $IMAGE_NAME:latest .
 docker push $IMAGE_NAME:latest
